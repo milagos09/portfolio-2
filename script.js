22f5f7598106af8663490eb5958ffbb88f8bac1e
unknown <milagos31@gmail.com>
2023-05-11 09:17:37 +0800

    added scroll effect for navbar

--- script.js
+++ script.js
@@ -20,8 +20,20 @@ function changeCarouselActive(event) {
     });
 }
 
+function fadeNavbarOnScroll() {
+    const navbar = document.querySelector("nav");
+    navbar.style.opacity = 0.25;
+    setTimeout(() => {
+        navbar.style.opacity = 1;
+    }, 2200);
+}
+
 document.getElementById("contactMeModal").addEventListener("submit", submitMessage);
 
 document.querySelectorAll(".dropdown-item").forEach((dropDown) => {
     dropDown.addEventListener("click", changeCarouselActive);
 });
+
+document.getElementById("year").innerText = new Date().getFullYear();
+
+window.addEventListener("scroll", fadeNavbarOnScroll);
